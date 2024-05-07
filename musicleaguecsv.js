5c76b406d84cea07399817c8c4b60a350f78f722
eriqjaffe <eriqjaffe@users.noreply.github.com>
2024-05-07 13:17:46 -0500

    Update musicleaguecsv.js
    Fixed for newer Music League site, also added the UTF-8 BOM to the CSV so diacritics render properly in Excel & Google Sheets.

--- musicleaguecsv.js
+++ musicleaguecsv.js
@@ -1,82 +1,70 @@
-//BucketHatBob 1/6/2023 modified eriqjaffe 2/6/2023
+//BucketHatBob 1/6/2023 modified eriqjaffe 5/7/2024
 
-var songs = Array.from(document.getElementsByClassName("rounded"));
+var songs = Array.from(document.getElementsByClassName("sticky-top"));
+var texts = Array.from(document.getElementsByClassName("text-body-tertiary"))
+var headers = Array.from(document.getElementsByClassName("card-title"))
 var playlistInfo = [];
 
-var roundHeader = songs[0].getElementsByClassName("d-block")
-var round = roundHeader[0].innerHTML.slice(6)
+var round = texts[0].innerHTML.slice(6)
 
 var roundNameHeader = songs[0].getElementsByTagName("h5")
-var roundName = roundNameHeader[0].innerHTML
+var roundName = headers[3].innerHTML
 
-songs.slice(1).forEach(song => {
+songs.slice(0).forEach(song => {
     var songInfo = {
         round: round,
       	roundName: '"'+roundName+'"',
-        name: "",
+        trackName: "",
         trackUrl: "",
         artist: "",
         album: "",
         submittedBy: "",
         votes: ""
     };
-    var dats = Array.from(song.getElementsByClassName("d-block"));
+    var dats = Array.from(song.getElementsByClassName("text-truncate"));
     var i = 0;
     var name, trackUrl, artist, album, sumbittedBy, votes;
+    
     dats.forEach(dat => {
-       
         var datEls = Array.from(dat.children);
+        var trackURI = dat.closest(".mb-4").id.split(":")
+        songInfo.trackUrl = '"https://open.spotify.com/track/'+trackURI[2]+'"'
         if (i == 0) {
-            songInfo.name = '"' + datEls[0].innerHTML + '"';
-            songInfo.trackUrl = datEls[0].href;
-        } else if (i == 1) {
-            songInfo.artist = '"' + datEls[0].innerHTML + '"';
-        } else if (i == 2) {
-            songInfo.album = '"' + datEls[0].innerHTML + '"';
-        } else if (i == 3) {
-            songInfo.votes = dat.innerHTML.slice(1);
+          var bar = Array.from(dat.parentElement.getElementsByClassName("text-end"))
+          songInfo.trackName = '"' + datEls[0].innerHTML + '"';
+          songInfo.artist = '"' + datEls[1].innerHTML + '"';
+          songInfo.album = '"' + datEls[2].innerHTML + '"';
+          songInfo.votes = bar[0].getElementsByClassName("m-0")[0].innerText
         } else if (i == 4) {
-            songInfo.submittedBy = dat.innerHTML.slice(13);
+          songInfo.submittedBy = '"' + datEls[0].innerHTML + '"';
         }
         i++;
     });
-    if (songInfo.name.length > 0) {
+    if (songInfo.trackName.length > 0) {
         playlistInfo.push(songInfo);
     }
 });
 
 console.table(playlistInfo);
 
-let csv = "round,roundName,trackName,trackURL,artist,album,submittedBy,votes\r\n"
-    // Loop the array of objects
-for (let row = 0; row < playlistInfo.length; row++) {
-    console.log(playlistInfo[row])
-    let keysAmount = Object.keys(playlistInfo[row]).length
-    let keysCounter = 0
-        // If this is the first row, generate the headings
-/*    if (row === 0) {
-        // Loop each property of the object
-        for (let key in playlistInfo[row]) {
-            // This is to not add a comma at the last cell
-            // The '\r\n' adds a new line
-            csv += key + (keysCounter + 1 < keysAmount ? ',' : '\r\n')
-            keysCounter++
-        }
-    } else { */
-        for (let key in playlistInfo[row]) {
-            csv += playlistInfo[row][key] + (keysCounter + 1 < keysAmount ? ',' : '\r\n')
-            keysCounter++
-        }
-/*    } */
-    keysCounter = 0
+var csvContent = "\ufeff";
+csvContent += "round,roundName,trackName,trackURL,artist,album,submittedBy,votes\r";
+for (var row = 0; row < playlistInfo.length; row++) {
+    csvContent += playlistInfo[row].round + ",";
+    csvContent += playlistInfo[row].roundName + ",";
+    csvContent += playlistInfo[row].trackName + ",";
+    csvContent += playlistInfo[row].trackUrl + ",";
+    csvContent += playlistInfo[row].artist + ",";
+    csvContent += playlistInfo[row].album + ",";
+    csvContent += playlistInfo[row].submittedBy + ",";
+    csvContent += playlistInfo[row].votes + "\r";
 }
 
-console.log(csv)
-
-// Once we are done looping, download the .csv by creating a link
-let link = document.createElement('a');
-link.id = 'download-csv';
-link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURI(csv));
+var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+var url = URL.createObjectURL(blob);
+var link = document.createElement('a');
+link.href = url;
 link.setAttribute('download', roundName+'.csv');
 document.body.appendChild(link);
-document.querySelector('#download-csv').click();
+link.click();
+URL.revokeObjectURL(url);
